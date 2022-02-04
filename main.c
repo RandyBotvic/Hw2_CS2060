aaa99411aa6d417f1881f0f107f2d521d134bf74
RandyBotvic <yedidya.brown@exit0r.com>
2022-02-04 11:46:00 -0700

    lessons learned

--- main.c
+++ main.c
@@ -1,15 +1,54 @@
+// Fig. 3.10: fig03_10.c
+// Analysis of examination results
 #include <stdio.h>
-void main()
+
+// function main begins program execution
+int main( void )
 {
-    int i,sum=0;
-    // Modified code off the interwebs to
-    // get the hang of writing my first loops
-    // just being honest ;/
-    printf("\nThe odd numbers are :");
-    for(i=1;i<=8;i++)
+    // initialize variables in definitions
+    unsigned int passes = 0; // number of passes
+    unsigned int failures = 0; // number of failures
+    unsigned int student = 1; // student counter
+    int result; // one exam result
+
+    // process 10 students using counter-controlled loop
+    do
     {
-        printf("%d ",2*i-1);
-        sum+=2*i-1;
-    }
-    printf("\nThe Sum of odd Natural Number 1 through 15 : %d \n",sum);
-}
+        // prompt user for input and obtain value from user
+        printf( "%s", "Enter result ( 1=pass,2=fail ): " );
+        int returnValue=scanf( "%d", &result );
+
+        // if result 1, increment passes
+        if ( returnValue == 1 && result == 1)
+        {
+            passes = passes + 1;
+            student = student + 1;
+        } // end if
+        else if (returnValue==1 && result == 2)
+        { // otherwise, increment failures
+            failures = failures + 1;
+            student = student + 1;
+        } // end else
+        else if (returnValue == 1)
+        {
+            puts("Please only enter prompted values!!");
+        }
+        else
+        {
+            while ((getchar())!='\n');
+            puts("Please only enter prompted values!!");
+        }
+        // increment student counter
+    }while ( student <= 10 );// end while
+
+    // termination phase; display number of passes and failures
+    printf( "Passed %u\n", passes );
+    printf( "Failed %u\n", failures );
+
+    // if more than eight students passed, print "Bonus to instructor!"
+    if ( passes > 8 )
+    {
+        puts( "Bonus to instructor!" );
+    } // end if
+} // end function main
+
