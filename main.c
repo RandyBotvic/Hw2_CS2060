2a676aa71ecd86219db0965ea9f0ded74443711b
RandyBotvic <yedidya.brown@exit0r.com>
2022-02-04 14:00:49 -0700

    lessons learned

--- main.c
+++ main.c
@@ -1,54 +1,36 @@
-// Fig. 3.10: fig03_10.c
-// Analysis of examination results
 #include <stdio.h>
 
-// function main begins program execution
-int main( void )
-{
-    // initialize variables in definitions
-    unsigned int passes = 0; // number of passes
-    unsigned int failures = 0; // number of failures
-    unsigned int student = 1; // student counter
-    int result; // one exam result
 
-    // process 10 students using counter-controlled loop
-    do
+
+
+    int main (void)
     {
-        // prompt user for input and obtain value from user
-        printf( "%s", "Enter result ( 1=pass,2=fail ): " );
-        int returnValue=scanf( "%d", &result );
 
-        // if result 1, increment passes
-        if ( returnValue == 1 && result == 1)
+        int startNumber;
+        int endNumber;
+        int sumOfOdds;
+
+        startNumber=0;
+        endNumber=0;
+        sumOfOdds=0;
+
+        printf( "%s", "Please enter starting number: ");
+        scanf("%d", &startNumber);
+
+        printf( "%s", "Please enter ending number: ");
+        scanf( "%d", &endNumber);
+
+
+
+        for (int i=startNumber; i<=endNumber; i++)
         {
-            passes = passes + 1;
-            student = student + 1;
-        } // end if
-        else if (returnValue==1 && result == 2)
-        { // otherwise, increment failures
-            failures = failures + 1;
-            student = student + 1;
-        } // end else
-        else if (returnValue == 1)
-        {
-            puts("Please only enter prompted values!!");
+            if(i%2==1)
+            {
+                sumOfOdds += i;
+            }
         }
-        else
-        {
-            while ((getchar())!='\n');
-            puts("Please only enter prompted values!!");
-        }
-        // increment student counter
-    }while ( student <= 10 );// end while
 
-    // termination phase; display number of passes and failures
-    printf( "Passed %u\n", passes );
-    printf( "Failed %u\n", failures );
-
-    // if more than eight students passed, print "Bonus to instructor!"
-    if ( passes > 8 )
-    {
-        puts( "Bonus to instructor!" );
-    } // end if
-} // end function main
+        printf( "This is the sum of all the 0dd Numbers:  %d", sumOfOdds);
+        // Start of methods used.
+    };
 
